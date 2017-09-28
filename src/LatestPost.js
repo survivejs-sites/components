9547f6ebf8b5b3689eb2676ec829122d24bf189e
Juho Vepsalainen <bebraw@gmail.com>
2017-09-28 10:01:14 +0200

    chore: Simplify LatestPost

--- src/LatestPost.js
+++ src/LatestPost.js
@@ -1,8 +1,8 @@
 import React from "react";
-import _ from "lodash";
+import Link from "./Link";
 
 const LatestPost = ({ section }) => {
-  const post = _.reject(section.pages("blog"), ({ isDraft }) => isDraft)[0];
+  const post = section.pages("blog")[0];
 
   if (!post) {
     return null;
@@ -11,13 +11,9 @@ const LatestPost = ({ section }) => {
   return (
     <div>
       <blockquote className="latestpost tip">
-        <a className="latestpost-link" href={`/${post.url}`}>
+        <Link className="latestpost-link" to={post.url}>
           {post.file.attributes.title}
-        </a>
-        <hr />
-        <a className="latestpost-link" href="/clinic/">
-          <b>Register</b> for Vienna clinics (late July, August)
-        </a>
+        </Link>
       </blockquote>
     </div>
   );
