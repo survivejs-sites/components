aa17dc0aa7ab0b3c18f3c413c3cbb1e9ee5ca94a
Juho Vepsalainen <bebraw@gmail.com>
2017-11-20 13:10:52 +0100

    chore(Navigation): Allow pathname to be checked

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,16 +1,23 @@
 import React from "react";
-import Link from "./Link";
+import { Link } from "@survivejs/components";
 
-const Navigation = ({ pages }) => (
+const Navigation = ({ pages, pathname }) => (
   <div className="nav__wrapper">
     <input type="checkbox" className="nav__toggle" id="nav__toggle" />
     <label className="nav__toggle-label" htmlFor="nav__toggle" />
     <nav className="nav">
-      {pages.map((link, i) => (
-        <div key={`link-${i}`} className="nav__link">
-          <Link to={link.url}>{link.title}</Link>
-        </div>
-      ))}
+      {pages.map(
+        (link, i) =>
+          link.url === pathname ? (
+            <div key={`link-${i}`} className="nav__selected__link">
+              {link.title}
+            </div>
+          ) : (
+            <div key={`link-${i}`} className="nav__link">
+              <Link to={link.url}>{link.title}</Link>
+            </div>
+          )
+      )}
     </nav>
   </div>
 );
